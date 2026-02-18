13566efb6126edb75bd17195900add07c18b34e2
Thais Mariano <86856151+thaisdevfront@users.noreply.github.com>
2026-02-17 22:49:01 -0300

    up - v1

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+ const items = document.querySelectorAll(".scroll-item");
+
+function handleScroll() {
+  const triggerBottom = window.innerHeight * 0.75;
+  const triggerTop = window.innerHeight * 0.2;
+
+  items.forEach(item => {
+    const box = item.getBoundingClientRect();
+
+    // ENTRADA
+    if (box.top < triggerBottom && box.bottom > triggerTop) {
+      item.classList.add("show");
+      item.classList.remove("hide");
+    }
+
+    // SAÍDA
+    else if (box.top <= triggerTop) {
+      item.classList.remove("show");
+      item.classList.add("hide");
+    }
+
+    else {
+      item.classList.remove("show", "hide");
+    }
+  });
+}
+
+window.addEventListener("scroll", handleScroll);
+handleScroll();
